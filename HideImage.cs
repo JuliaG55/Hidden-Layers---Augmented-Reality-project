47015587e8c9959b1c86b36cefa80b3edae7c605
Julia Gabajova <34511349+JuliaG55@users.noreply.github.com>
2019-06-10 19:00:27 +0100

    Add files via upload
    Unfortunately can't upload the whole project as it is large file. If you would like any advice or would like to see it I am happy to help or send it to you. Just drop me an email julia.gabajova@gmail.com

---
+++ HideImage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HideImage : MonoBehaviour {
+    public GameObject OriginalImage;
+    private int moving;
+    private float step;
+    private readonly float stepCount = 100F;
+
+    public void Update()
+    {
+        if (moving == 1 && step < stepCount)
+        {
+            step++;
+            float stepsLeft = stepCount - step;
+            float opacity = stepsLeft / stepCount;
+            if (OriginalImage.transform.rotation.x < 180)
+            {
+                Debug.Log(OriginalImage.transform.rotation.x);
+                OriginalImage.transform.Rotate(5, 0, 0);
+            }
+            OriginalImage.transform.position += new Vector3(1f, 0f, 0f);
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        moving = 1;
+    }
+
+    public void OnEnable()
+    {
+        Debug.Log("Enabled");
+    }
+}
